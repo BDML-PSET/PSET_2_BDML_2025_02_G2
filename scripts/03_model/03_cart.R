83b4cbd9374d7909e386bb2a9c759b56d72a1d85
AngelCas20 <angelcastillo2060@gmail.com>
2025-10-17 21:15:49 -0500

    push rest of original data

--- scripts/03_model/03_cart.R
+++ scripts/03_model/03_cart.R
@@ -19,3 +19,47 @@ df_split = initial_validation_split(data = train,prop = c(0.6,0.2),strata = pobr
 df_train = training(df_split)
 df_validation = validation(df_split)
 df_test = testing(df_split)
+
+#---------------#
+# 3. receta     #
+#---------------#
+
+# Definir control de entrenamiento (validación cruzada 10-fold)
+
+multiStats <- function(...) c(twoClassSummary(...), defaultSummary(...), prSummary(...))
+
+ctrl <- trainControl(method = "cv",  
+                     number = 5,  
+                     summaryFunction = multiStats,  
+                     classProbs = TRUE,  
+                     verbose = TRUE,  
+                     savePredictions = TRUE)
+
+set.seed(4326)
+model = train(pobre ~ .,
+              data = df_train,
+              method = "rpart",
+              metric = "F",
+              trControl = ctrl,
+              tuneLength = 50)
+
+#-----------------------------#
+# 4. Evaluación del modelo    #
+#-----------------------------#
+predictions_df_test = df_test %>% 
+                      mutate(poor = predict(model,df_test)) %>% 
+                      select(poor, pobre)
+
+f_meas(data = predictions_df_test,truth = pobre,estimate = poor)
+
+# Predicciones sobre test - kaggle
+predictions_bayes = test %>% 
+                    mutate(pobre = predict(model,test),
+                           pobre = ifelse(pobre == 'Yes',1,0)) %>% 
+                    select(id,pobre)
+
+#-------------------#
+# 5. Exportar datos #
+#-------------------#
+export(model, "stores/output/03_models/03_model_cart.rds")
+export(predictions_bayes, "stores/output/03_models/03_cart_predictions.csv")
